5300afcec078b0921b2a459f2eb040940cfa5aac
J Dasenbrock <dasenbrockjw@gmail.com>
2017-03-22 10:03:36 -0500

    shutting down project for now

--- create_dataframe.py
+++ create_dataframe.py
@@ -1,24 +1,42 @@
 """Create dataframe with zipcode, GPS coords. and city name"""
 import pandas as pd
-import zipcode
 import pickle_funcs as pk
+import zipcode
+from pyzipcode import ZipCodeDatabase
+from geopy.geocoders import Nominatim
 
-def get_city_data(zipc):
+def get_city_data_geopy(zipc):
     """Given a zip code, return latlon and city name"""
-    data = zipcode.isequal(str(zipc))
+    geolocator = Nominatim()
+    coord_data = geolocator.geocode(str(zipc))
+    name_data = zipcode.isequal(str(zipc))
 
-    if data is not None:
-        lat = data.lat
-        lon = data.lon
-        name = '%s, %s' % (data.city, data.state)
+    if coord_data is not None and name_data is not None:
+        lat = coord_data[1][0]
+        lon = coord_data[1][1]
+        name = '%s, %s' % (name_data.city, name_data.state)
         return lat, lon, name
     # Some data points are empty, return string of none in this case
     else:
         return 'None', 'None', 'None'
 
+def get_city_data(zipc):
+    """Given a zip code, return latlon and city name"""
+    geolocator = ZipCodeDatabase()
+    try:
+        zip_data = geolocator[str(zipc)]
+        lat = zip_data.latitude
+        lon = zip_data.longitude
+        name = '%s, %s' % (zip_data.city, zip_data.state)
+        return lat, lon, name
+
+    except:
+        return 'None', 'None', 'None'
+
 
 def strip_chars(string):
     """Returns a string with only ints"""
+    string = str(string)
     newstring = ''
 
     for k in string:
@@ -46,27 +64,40 @@ def create_dataframe(csvfilename):
     
     # Read CSV file as a dataframe
     data = pd.read_csv('%s.csv' % csvfilename)
-    data = pull_numbers(data, 'zipcode')
+    print('data read')
 
-    # Create blank columns in the dataframe
-    blanks = []
-    for k in range(len(data)):
-        blanks.append('None')
-    data['lats'] = pd.Series(blanks)
-    data['lons'] = pd.Series(blanks)
-    data['city names'] = pd.Series(blanks)
+    lat_dict = {}
+    lon_dict = {}
+    name_dict = {}
 
-    # Fill in city data
-    for k in range(len(data.index)):
-        city_data = get_city_data(data['zipcode'][k])
-        data['lats'][k] = city_data[0]
-        data['lons'][k] = city_data[1]
-        data['city names'][k] = city_data[2]
+    data['zipcode'] = data['zipcode'].apply(strip_chars)
+    print('zipcode modified')
+
+    for zipc in data['zipcode']:
+        city_data = get_city_data(zipc)
+        lat_dict[zipc] = city_data[0]
+        lon_dict[zipc] = city_data[1]
+        name_dict[zipc] = city_data[2]
+
+    print('looked up zipcode data')
+
+    def dict_lookup(zipcode, dict_name):
+        return dict_name[zipcode]
+
+    data['lats'] = data['zipcode'].apply(dict_lookup, args=(lat_dict,))
+    print('input lats data')
+    data['lons'] = data['zipcode'].apply(dict_lookup, args=(lon_dict,))
+    print('input lons data')
+    data['city names'] = data['zipcode'].apply(dict_lookup, args=(name_dict,))
+    print('input city name data')
+
 
     # Create pickle object from the dataframe
-    pk.pickle_object(data, 'city_coords', test=False)
+    pk.pickle_object(data, 'error_data', test=False)
 
     return data
+
+
 
 
 
