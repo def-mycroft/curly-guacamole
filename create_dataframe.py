71df988cc1eb1884b1e2a5db8632f35b8229fb89
J Dasenbrock <dasenbrockjw@gmail.com>
2017-03-21 19:48:03 -0500

    before starting to implement google maps api for zip lookup

--- create_dataframe.py
+++ create_dataframe.py
@@ -17,11 +17,36 @@ def get_city_data(zipc):
         return 'None', 'None', 'None'
 
 
-def map_zipcode(csvfilename):
+def strip_chars(string):
+    """Returns a string with only ints"""
+    newstring = ''
+
+    for k in string:
+
+        try:
+            a = int(k)
+            newstring = newstring + k
+        except ValueError:
+            pass
+
+    return newstring # Must return string due to leading zeros
+
+
+def pull_numbers(dataframe, column_name):
+    """Extracts numbers from members of a column"""
+
+    for k in range(len(dataframe)):
+        dataframe[column_name][k] = strip_chars(dataframe[column_name][k])
+
+    return dataframe
+
+
+def create_dataframe(csvfilename):
     """Create a dataframe with zipcodes, GPS coords., and population"""
     
     # Read CSV file as a dataframe
-    data = pd.read_csv('%s.csv', csvfilename)
+    data = pd.read_csv('%s.csv' % csvfilename)
+    data = pull_numbers(data, 'zipcode')
 
     # Create blank columns in the dataframe
     blanks = []
@@ -31,26 +56,17 @@ def map_zipcode(csvfilename):
     data['lons'] = pd.Series(blanks)
     data['city names'] = pd.Series(blanks)
 
-    latitudes = []
-    longitudes = []
-    city_names = []
-
-    # Get city data for each zip code
-    for zipc in data['Zip Code ZCTA']:
-        data_tuple = get_city_data(zipc)
-        latitudes.append(data_tuple[0])
-        longitudes.append(data_tuple[1])
-        city_names.append(data_tuple[2])
-
-    # TODO This is inefficient, should be able to use one loop
-    # Input city data into the dataframe
-    for k in range(len(data)):
-        data.iloc[k,2] = latitudes[k]
-        data.iloc[k,3] = longitudes[k]
-        data.iloc[k,4] = city_names[k]
+    # Fill in city data
+    for k in range(len(data.index)):
+        city_data = get_city_data(data['zipcode'][k])
+        data['lats'][k] = city_data[0]
+        data['lons'][k] = city_data[1]
+        data['city names'][k] = city_data[2]
 
     # Create pickle object from the dataframe
     pk.pickle_object(data, 'city_coords', test=False)
+
+    return data
 
 
 
